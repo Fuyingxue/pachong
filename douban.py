08241258b09d62fa0813a8b6b879fd3e8fde97e2
Fuyingxue <xia_chuanhui@163.com>
2018-07-12 23:19:48 +0800

    xiug

--- douban.py
+++ douban.py
@@ -3,14 +3,29 @@ import requests
 from pyquery import PyQuery as pq
 
 
+# 自定义log，代替print
 def log(*args, **kwargs):
     print(*args, **kwargs)
+
+
+# 可以伪装客户端及登录
+# 替换 cookie
+cookie = ''
+accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+useragent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36'
+
+headers = {
+    'Cookie': cookie,
+    'User-Agent': useragent,
+    'Accept': accept,
+}
 
 
 class Model(object):
     """
     基类, 用来显示类的信息
     """
+
     def __repr__(self):
         name = self.__class__.__name__
         properties = ('{}=({})'.format(k, v) for k, v in self.__dict__.items())
@@ -22,6 +37,7 @@ class Movie(Model):
     """
     存储电影信息
     """
+
     def __init__(self):
         self.name = ''
         self.score = 0
@@ -36,7 +52,6 @@ def cached_url(url):
     """
     folder = 'cached'
     filename = url.split('=', 1)[-1] + '.html'
-    'cached/0.html'
     path = os.path.join(folder, filename)
     if os.path.exists(path):
         with open(path, 'rb') as f:
@@ -47,10 +62,6 @@ def cached_url(url):
         if not os.path.exists(folder):
             os.makedirs(folder)
 
-        headers = {
-            'user-agent': '''Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8''',
-        }
         # 发送网络请求, 把结果写入到文件夹中
         r = requests.get(url, headers)
         with open(path, 'wb') as f:
@@ -78,9 +89,9 @@ def movie_from_div(div):
 def movies_from_url(url):
     """
     从 url 中下载网页并解析出页面内所有的电影
-    
+
     只下载一次
-    
+
     1. 解析 dom
     2. 找到父亲节点
     3. 每个子节点拿一个movie
@@ -98,18 +109,6 @@ def movies_from_url(url):
 
 
 def download_image(url, name):
-    # 可以伪装客户端及登录
-    # 替换 cookie
-    cookie = ''
-    accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
-    useragent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36'
-
-    headers = {
-        'Cookie': cookie,
-        'User-Agent': useragent,
-        'Accept': accept,
-    }
-
     folder = "img"
     names = name.split("/")[0] + '.jpg'
     path = os.path.join(folder, names)
@@ -121,7 +120,7 @@ def download_image(url, name):
         return
 
     # 发送网络请求, 把结果写入到文件夹中
-    r = requests.get(url, headers=headers)
+    r = requests.get(url, headers)
     with open(path, 'wb') as f:
         f.write(r.content)
 
